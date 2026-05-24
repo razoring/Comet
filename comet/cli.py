81f99be2130fdb5c8fc2cd4d6e797f887e43f807
raymondso123 <blotzytgaming@gmail.com>
2026-05-23 20:56:33 -0400

    feat: initialize CLI application with basic Ollama model interaction and project dependencies

--- comet/cli.py
+++ comet/cli.py
@@ -1,2 +1,13 @@
+from httpx import __name
+from ollama import chat, ChatResponse, Client
+import argparse
+
 def main():
-    print("Comet cli running.")
+    client = Client()
+    #print(client.list())
+    models = sorted(client.list().models, key=lambda m:m.size, reverse=False)
+
+    response:ChatResponse = chat(model=models[0].model, messages=[{"role":"system", "message":"You are an assistant."},{"role":"user", "message":"Hello World!"}], think=False, keep_alive=0)
+    print(response.message.content)
+
+if __name__ == "__main__": main()
